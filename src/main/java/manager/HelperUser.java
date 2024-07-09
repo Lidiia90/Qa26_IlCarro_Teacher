f7208dc705d5f73eda8e2e8555893edcfc24b15b
Lidiia90 <liduska200412@gmail.com>
2024-07-09 21:05:23 +0300

    registration success & lombok

--- src/main/java/manager/HelperUser.java
+++ src/main/java/manager/HelperUser.java
@@ -1,6 +1,8 @@
 package manager;
 
+import models.User;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
@@ -14,17 +16,23 @@ public class HelperUser extends HelperBase {
 
     }
 
-
     public void fillLoginForm(String email, String password) {
         type(By.id("email"), email);
         type(By.id("password"), password);
     }
 
-    public void submitLogin() {
+    //overloading
+    public void fillLoginForm(User user) {
+        type(By.id("email"), user.getEmail());
+        type(By.id("password"), user.getPassword());
+    }
+
+    public void submit() {
         click(By.xpath("//*[@type='submit']"));
     }
 
     public void clickOkButton() {
+        if (isElementPresent(By.xpath("//button[text()='Ok']")))
         click(By.xpath("//button[text()='Ok']"));
     }
 
@@ -43,5 +51,40 @@ public class HelperUser extends HelperBase {
 
     public void logout() {
         click(By.xpath("//*[text()=' Logout ']"));
+    }
+
+    public String getErrorText() {
+        return wd.findElement(By.cssSelector("div.error")).getText();
+    }
+
+    public boolean isYallaButtonNotActive() {
+        boolean res = isElementPresent(By.cssSelector("button[disabled]"));
+
+        WebElement element = wd.findElement(By.cssSelector("button[type='submit']"));
+       boolean result = element.isEnabled();
+
+       return res && !result;
+    }
+    //****************Registration**********
+
+    public void openRegistrationForm(){
+        click(By.xpath("//*[text()=' Sign up '] "));
+    }
+    public void fillRegistrationForm(User user){
+        type(By.id("name"),user.getName());
+        type(By.id("lastName"), user.getLastName());
+        type(By.id("email"), user.getEmail());
+        type(By.id("password"), user.getPassword());
+    }
+    public void checkPolicy(){
+      //  click(By.id("terms-of-use")); 0*0
+
+        //variant2
+       // click(By.cssSelector("label[for='terms-of-use']"));
+
+        //variant3
+        JavascriptExecutor js = (JavascriptExecutor) wd;
+        js.executeScript("document.querySelector('#terms-of-use').click()");
+
     }
 }
